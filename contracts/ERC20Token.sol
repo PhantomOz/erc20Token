c1896b32ede901eb099dfc7876c54448a8f66a8f
PhantomOz <faniogor@gmail.com>
2024-02-14 01:51:09 +0100

    This commit adds some state private variable and assigns them with a contructor

--- contracts/ERC20Token.sol
+++ contracts/ERC20Token.sol
@@ -1,4 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-contract ERC20Token {}
+contract ERC20Token {
+    string private s_name;
+    string private s_symbol;
+    uint256 private s_totalSupply;
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint256 _totalSupply
+    ) {
+        s_name = _name;
+        s_symbol = _symbol;
+        i_totalSupply = _totalSupply;
+    }
+}
